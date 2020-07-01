5b1ddf569720b0195fb702be8e504b50bc8fe962
Balamurugan S <36988435+balamurugan-shanmuganathan@users.noreply.github.com>
2020-07-01 16:04:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,109 +1,33 @@
-from flask import Flask, render_template, request, url_for, redirect
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
+from ReviewScraper.ReviewScraper import reviewScraper
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    img1 = os.path.join('static','flipkartImg', 'Image1.jpg')
+    return render_template('index.html',img1 = img1)
 
-@app.route('/result', methods = ['GET','POST'])
-def result():
+@app.route('/about')
+def about():
+    img2 = os.path.join('static','flipkartImg', 'Image2.jpg')
+    return render_template('about.html', img2 = img2)
+
+@app.route('/Reviews', methods = ['GET','POST'])
+def reviews():
     if request.method == 'POST':
-
-        searchString = request.form['content'].replace(" ","")
         try:
-
-            # Get Product URL
-            flipkartUrl = 'https://www.flipkart.com/search?q=' + searchString
-            reqData = requests.get(flipkartUrl)
-            flipkartHtml = BeautifulSoup(reqData.content, 'html.parser')
-            # flipkartLink = flipkartHtml.find_all('div', {'class': 'bhgxx2 col-12-12'})
-
-            productLink = []
-            baseLink = "https://www.flipkart.com"
-            for i in flipkartHtml.findAll('a'):
-                try:
-                    if len(i.get('href')) > 100:
-                        link = baseLink + i.get('href')
-                    productLink.append(link)
-                except:
-                    'None'
-            productLink = list(dict.fromkeys(productLink))  # Remove duplicate links
-
-            
-            results = []
-
-            for link in productLink[0:10]:
-
-                productPage = requests.get(link)
-                product = BeautifulSoup(productPage.content, 'html.parser')
-
-                # ProductImage
-                try:
-                    prodImage = str(product.find('div', {'class': '_2_AcLJ'})).split('(')[1].replace(')"></div>',
-                                                                                                     '')
-                except:
-                    prodImage = ''
-
-                # ProductName
-                try:
-                    name = product.find_all('span', {'class': '_35KyD6'})
-                    prodName = name[0].text
-                except:
-                    prodName = ''
-
-                # Ratings
-                try:
-                    rating = product.find('div', {'class': 'hGSR34'})
-                    prodRatings = rating.get_text()
-                except:
-                    prodRatings = ''
-
-                # Price
-                try:
-                    price = product.find('div', {'class': '_3auQ3N _1POkHg'})
-                    prodPrice = price.get_text()
-                except:
-                    prodPrice = ''
-
-                # Offer
-                try:
-                    offer = product.find_all('div', {'class': 'VGWI6T _1iCvwn'})
-                    prodOffer = offer[0].text
-                except:
-                    prodOffer = ''
-
-                # Offer Price
-                try:
-                    offerPrice = product.find('div', {'class': '_1vC4OE _3qQ9m1'})
-                    prodOfferPrice = offerPrice.get_text()
-                except:
-                    prodOfferPrice = ''
-
-                # Highlights
-                try:
-                    highlights = product.find('div', {'class': '_3WHvuP'})
-                    prodHighlights = ' | '.join([e.text for e in highlights.find_all('li')])
-                except:
-                    prodHighlights = ''
-
-                mydict = {"Product": searchString,
-                          "Image": prodImage,
-                          "Name": prodName,
-                          "Rating": prodRatings,
-                          "Price": prodPrice,
-                          "Offer": prodOffer,
-                          "OfferPrice": prodOfferPrice,
-                          "Highlights": prodHighlights}
-                results.append(mydict)                
-
-
-            return render_template('result.html', results = results)
+            url = request.form['url']
+            pages = int(request.form['page'])        
+            soup = reviewScraper.urlParser(url)
+            prodInfo = reviewScraper.prodInformation(soup)
+            pageLink = reviewScraper.pageLink(soup, pages)
+            reviews = reviewScraper.productReview(pageLink)
+            review_len  = '{} Reviews are scraped from {} pages'.format(reviews.shape[0],pages)
+            return render_template('reviews.html', prodInfo = prodInfo ,reviews = reviews, prodImg = prodInfo['Image'].values, review_len = review_len)
         except:
-            return "Something Wrong"
-
-
+            return 'Can not parse this link. Please try another link or refer About Page'
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run(debug = True)
